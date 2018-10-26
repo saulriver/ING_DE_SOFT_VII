283c4844fe17b5aa27423fea4f51920c1cb15147
saulriver <sarima666@gmail.com>
2018-10-26 01:09:43 -0500

    instalacion gema devise y creacion de user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :egresados
   resources :facultades
   resources :programas
-  root 'egresados#index'
+  root :to => 'egresados#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
