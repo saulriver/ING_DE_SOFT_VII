b3cbdd00b09a527400f8ac12745a64440c8ca920
saulriver <42225276+saulriver@users.noreply.github.com>
2018-11-06 19:55:24 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  
 resources :welcome, only: [:index]
-
+root 'welcome#index'
   resources :egresados
   resources :facultades
   resources :programas
-  root 'welcome#index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
